5407fd21d92cef11a77a4bc2028c3ec1a29f2197
df6ty4bit <stephengodwin895@gmail.com>
2025-05-20 23:34:40 +0100

    Initial form

--- src/components/Login/Signin.tsx
+++ src/components/Login/Signin.tsx
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import email_icon from "../../../src/assets/email.png";
 import user_icon from "../../../src/assets/person.png";
 import password_icon from "../../../src/assets/person.png";
 import "../../../src/components/Login/Signin.css";
 
 const Signin = () => {
+    const [action, setAction] = useState("Sign Up");
     return (
         <>
             <form action="">
                 <div className="container">
                     <div className="header">
-                        <div className="text">Sign Up</div>
+                        <div className="text">{action}</div>
 
                         <div className="underline"></div>
                     </div>
                     <div className="inputs">
-                        <div className="input">
-                            <img src={user_icon} alt="" />
+                        {action === "Login" ? (
+                            <div></div>
+                        ) : (
+                            <div className="input">
+                                <img src={user_icon} alt="" />
 
-                            <input type="text" placeholder="Name" />
-                        </div>
+                                <input type="text" placeholder="Name" />
+                            </div>
+                        )}
 
                         <div className="input">
                             <img src={email_icon} alt="" />
@@ -33,12 +38,31 @@ const Signin = () => {
                             <input type="password" placeholder="Password" />
                         </div>
                     </div>
-                    <div className="forgot-password">
+                    {action==="Sign Up"?<div></div>: <div className="forgot-password">
                         Forgot Password? <span>Cick Here</span>
                     </div>
+                   }
                     <div className="submit-container">
-                        <div className="submit">SignIn</div>
-                        <div className="submit">SignUp</div>
+                        <div
+                            className={
+                                action === "Login" ? "submit gray" : "submit"
+                            }
+                            onClick={() => {
+                                setAction("Sign Up");
+                            }}
+                        >
+                            Sign Up
+                        </div>
+                        <div
+                            className={
+                                action === "Sign Up" ? "submit gray" : "submit"
+                            }
+                            onClick={() => {
+                                setAction("Login");
+                            }}
+                        >
+                            Login
+                        </div>
                     </div>
                 </div>
             </form>
