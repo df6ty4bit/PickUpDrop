ae131398bbfb33e7a2a8c8550cf8455e052f89e4
df6ty4bit <stephengodwin895@gmail.com>
2025-05-20 23:02:32 +0100

    Initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Signin from "./components/Login/Signin";
 import React from "react";
+import "../src/Index.css"
 
 const App = () => {
     return (
--- src/components/Login/Signin.tsx
+++ src/components/Login/Signin.tsx
@@ -1,13 +1,47 @@
 import React from "react";
+import email_icon from "../../../src/assets/email.png";
+import user_icon from "../../../src/assets/person.png";
+import password_icon from "../../../src/assets/person.png";
+import "../../../src/components/Login/Signin.css";
 
 const Signin = () => {
     return (
         <>
-            <div className="container">
-                <form action="">
-                
-                </form>
-            </div>
+            <form action="">
+                <div className="container">
+                    <div className="header">
+                        <div className="text">Sign Up</div>
+
+                        <div className="underline"></div>
+                    </div>
+                    <div className="inputs">
+                        <div className="input">
+                            <img src={user_icon} alt="" />
+
+                            <input type="text" placeholder="Name" />
+                        </div>
+
+                        <div className="input">
+                            <img src={email_icon} alt="" />
+
+                            <input type="email" placeholder="Email" />
+                        </div>
+
+                        <div className="input">
+                            <img src={password_icon} alt="" />
+
+                            <input type="password" placeholder="Password" />
+                        </div>
+                    </div>
+                    <div className="forgot-password">
+                        Forgot Password? <span>Cick Here</span>
+                    </div>
+                    <div className="submit-container">
+                        <div className="submit">SignIn</div>
+                        <div className="submit">SignUp</div>
+                    </div>
+                </div>
+            </form>
         </>
     );
 };
